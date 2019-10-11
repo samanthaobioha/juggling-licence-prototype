87bd6c2b88f43bc7b114886c10a9544292f92c14
Samantha <samantha@Administrators-MacBook-Pro.local>
2019-10-11 15:03:39 +0100

    added routing for cant juggle

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,18 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Branching
+router.post('/juggling-trick', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let canjuggle = req.session.data['how-many-balls']
+
+  if (canjuggle === 'None - I cannot juggle') {
+    res.redirect('/cant-juggle')
+  } else {
+    res.redirect('/juggling-trick')
+  }
+})
 module.exports = router
